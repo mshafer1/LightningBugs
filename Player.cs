b49132236f2af276772e9beefa4d347d87287e62
Nehemiahl <elopez@harding.edu>
2013-11-21 20:29:14 -0600

    have all turns but right to down working

--- Player.cs
+++ Player.cs
@@ -13,7 +13,8 @@ namespace LightningBugs
     public partial class Player : GameImage
     {
 
-        public Player(Image image): base(image)
+        public Player(Image image)
+            : base(image)
         {
             InitializeComponent();
             direction = new DrirectionClass();
@@ -27,67 +28,117 @@ namespace LightningBugs
 
         }
 
+        private int ceil(float input)
+        {
+            int result = (int)input;
+            if (result - input > 0)
+            {
+                result++;
+            }
+            return result;
+        }
+
+        private int divideByFourWithRounding(int input)
+        {
+            int result = (int)((float)((input / 4 + .5)));
+            return result;
+
+        }
+
         public virtual void turn(Direction d)
         {
-            while (direction.CompareTo(d) != 0)
-            { int temp = Width;
-
-                KeyValuePair<int, int> pos = getPosition();
+            //If the direction of the player is not equal to the direction the player wants to move to
+            while (direction.getDirection() != d)
+            {
+                //Getting actual position of center
+                KeyValuePair<int, int> pos = getTrunkPosition();
                 int x = pos.Key;
-                int y = (pos.Value);
+                int y = pos.Value;
 
-
-                pos = getTrunkPosition();
-                int x0 = pos.Key;
-                int y0 = pos.Value;
+                //Rotating 90 degrees clockwise
                 
+
+
+                //Adjusting the top and left so player has the same center as before.
+                //Top = y - (Height / 2);
+                //Left = x - (Width / 2);
+
+                switch (direction.getDirection())
+                {
+                    // up going right
+                    case (Direction.up):
+                        Left = x - divideByFourWithRounding(this.Width);
+                        Top = y - divideByFourWithRounding(this.Height);
+                        break;
+                    case (Direction.down):
+                        Left = x + divideByFourWithRounding(this.Width) - this.Height;
+                        Top = y - divideByFourWithRounding(this.Height);
+                        break;
+                    case (Direction.left):
+                        Left = x - divideByFourWithRounding(this.Width);
+                        Top = y + divideByFourWithRounding(this.Height) - this.Width;
+                        break;
+                    case (Direction.right):
+                        Left = x - divideByFourWithRounding(this.Height);
+                        Top = y + divideByFourWithRounding(this.Height) - (this.Width);
+                        break;
+                }
+                Image.RotateFlip(RotateFlipType.Rotate270FlipXY);
+                int temp = this.Width;
+                this.Width = this.Height;
+                this.Height = temp;
+
+
+                direction.decrement();
+                //pos = getTrunkPosition();
+                //int x0 = pos.Key;
+                //int y0 = pos.Value;
+
                 //if (direction.CompareTo(d) < 0)
                 //{
-                   Image.RotateFlip(RotateFlipType.Rotate270FlipXY);
-                    //if (direction.getDirection() == Direction.down)
-                    //{
-                    //    x -= Height / 2;
-                    //}
-                   direction.decrement();
+
+                //if (direction.getDirection() == Direction.down)
+                //{
+                //    x -= Height / 2;
+                //}
+
                 //}
                 //else
                 //{
-                    //Image.RotateFlip(RotateFlipType.Rotate90FlipXY);
-                    //if (direction.getDirection() == Direction.up)
-                    //{
-                    //    x += Height / 2;
-                    //}
-                    //direction.increment();
+                //Image.RotateFlip(RotateFlipType.Rotate90FlipXY);
+                //if (direction.getDirection() == Direction.up)
+                //{
+                //    x += Height / 2;
                 //}
-                Width = Height;
-                Height = temp;
+                //direction.increment();
+                //}
+
 
                 //KeyValuePair<int, int> posFinal = getTrunkPosition();
-                Top = y - (Height / 2);
-                Left = x - (Width / 2);
 
-                Left = x0 - (int)((float)Width / 2);
-                Top = y0 - Height;
+
+                //Left = x0 - (int)((float)Width / 2);
+                //Top = y0 - Height;
             }
         }
 
         public void move(GameImage trail)
         {
-            int moveLength = this.length/2;
-            
+            int moveLength = this.length / 2;
+
             trail.Height = moveLength;
             if (this.direction.getDirection() == Direction.up)
             {
-                trail.Top = (int)(this.Top + Height-moveLength);//Height - moveLength is position from front of car that trail should end before car is moved
+                trail.Top = (int)(this.Top + Height - moveLength);//Height - moveLength is position from front of car that trail should end before car is moved
                 trail.Left = (int)(this.Left + (float)(this.Width - trail.Width) / 2);//by dividing the diference of the widths by two, we get half of the margin to put on one side.
-                
+
                 this.Top -= (int)(moveLength);
             }
             else if (this.direction.getDirection() == Direction.down)
             {
                 trail.Top = (int)(this.Top);
                 trail.Left = (int)(this.Left + (float)(this.Width - trail.Width) / 2);
-                
+
                 this.Top += (int)(moveLength);
             }
             else
@@ -101,13 +152,13 @@ namespace LightningBugs
                 if ((this.direction.getDirection() == Direction.left))
                 {
                     trail.Left = this.Left + this.Width - moveLength;
-                    
+
                     this.Left -= (int)(moveLength);
                 }
                 else
                 {
                     trail.Left = this.Left;
-                    
+
                     this.Left += (int)(moveLength);
                 }
             }
@@ -120,7 +171,8 @@ namespace LightningBugs
 
         public KeyValuePair<int, int> getPosition()
         {
-            KeyValuePair<int, int> result = new KeyValuePair<int, int>(Left + Width / 2, Top + Height / 2);
+            KeyValuePair<int, int> result = new KeyValuePair<int, int>(Left + (Width / 2), Top + (Height / 2));
+
             return result;
         }
 
@@ -150,20 +202,20 @@ namespace LightningBugs
 
         public KeyValuePair<int, int> getFrontPosition()
         {
-            KeyValuePair<int, int> result = new KeyValuePair<int,int>();
+            KeyValuePair<int, int> result = new KeyValuePair<int, int>();
             switch (direction.getDirection())
             {
-                case(Direction.up):
+                case (Direction.up):
                     result = new KeyValuePair<int, int>(Left + Width / 2, Top);
                     break;
-                case(Direction.down):
+                case (Direction.down):
                     result = new KeyValuePair<int, int>(Left + Width / 2, Top + Height);
                     break;
-                case(Direction.left):
+                case (Direction.left):
                     result = new KeyValuePair<int, int>(Left, Top + Height / 2);
                     break;
-                case(Direction.right):
-                    result = new KeyValuePair<int, int>(Left+Width, Top + Height /2);
+                case (Direction.right):
+                    result = new KeyValuePair<int, int>(Left + Width, Top + Height / 2);
                     break;
             }
             return result;
